1d8215ffebf2f61bbe3ae08d1d3af9a79cfc3b9d
xaxys <tpnnghd@163.com>
2023-11-29 11:18:18 +0800

    Add GitHub Actions and GoReleaser
    Signed-off-by: xaxys <tpnnghd@163.com>

--- tools/antlr.sh
+++ tools/antlr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cur_dir=$(dirname "$0")
-CLASSPATH=".:$cur_dir/antlr-4.13.1-complete.jar:$CLASSPATH"
-TEST_CURRENT_DIR=${CLASSPATH//.:}
+export CLASSPATH=".:$cur_dir/antlr-4.13.1-complete.jar:$CLASSPATH"
+export TEST_CURRENT_DIR=${CLASSPATH//.:}
 if [ "$TEST_CURRENT_DIR" == "$CLASSPATH" ]; then
-    CLASSPATH=".:$CLASSPATH"
+    export CLASSPATH=".:$CLASSPATH"
 fi
 java org.antlr.v4.Tool "$@"
